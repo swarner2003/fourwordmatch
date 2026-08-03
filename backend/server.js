cf56f94b7bb08fd0f7616afaf91100bfd88217ae
swarner2003 <swarner2022@my.fit.edu>
2026-08-03 12:05:05 -0400

    base backend install

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+const corsOptions = {
+    origin: ["http://localhost:5173/"]
+}
+
+app.use(cors(corsOptions));
+
+app.listen(8080, () => {
+    console.log("Server started on port 8080")
+})
