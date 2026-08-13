2750d0dad237ef037681cdf0c03e351f53245bad
swarner2003 <swarner2022@my.fit.edu>
2026-08-13 15:25:51 -0400

    added profile design + list of the 10 most recent connection games the user has made

--- four-word-match/src/Profile.jsx
+++ four-word-match/src/Profile.jsx
@@ -21,9 +21,9 @@ function Profile(){
 
     useEffect(() => {
         if (!isLoading && isAuthenticated && user?.sub && user?.nickname) {
-            fetchAPI(user.sub, user?.nickname);
+            fetchAPI(user.sub, user.nickname);
         }
-    }, [isLoading, isAuthenticated, user]);
+    }, [isLoading, isAuthenticated, user?.sub, user?.nickname]);
 
     if (!isAuthenticated) {
         return (
@@ -40,10 +40,20 @@ function Profile(){
     return (
         isAuthenticated && (
             <div>
-                <h1 className="flex justify-center items-center text-2xl">Welcome to profile</h1>
-                <h1 className="flex justify-center items-center text-xl">User ID: {user.sub}</h1>
-                <h1 className="flex justify-center items-center text-xl">User Name: {user.nickname}</h1>
-                <h1 className="flex justify-center items-center text-xl">Data: {data?.ownedGames?.[0]?.TableName ?? "Loading..."}</h1>
+                <h1 className="flex justify-center items-center text-6xl mb-4">Profile</h1>
+                <h1 className="flex justify-center items-center text-3xl mb-4">{user.nickname}</h1>
+                <h1 className="flex justify-center items-center text-xl mb-6 underline">Most Recently Made Connections</h1>
+
+                <div className="flex flex-wrap w-150 mx-auto mt-4">
+                    {data?.ownedGames?.slice(0, 10).map((game, index) =>(
+                        <div className='w-150 flex items-center p-2 mb-4 bg-stone-200 rounded-lg ml-auto'>
+                            <h1 key={game.TableID ?? index}>{index + 1}. {game.TableName}</h1>
+                            <a className='ml-auto' href={`/${game.TableID}`}>http://localhost:5173/{game.TableID}</a>
+                        </div>
+                    ))}
+                </div>
+
+
                 <div className="flex justify-center items-center">
                     <LogoutButton />
                 </div>
