366551c0dc45aa41947ed8db7cb8c5b82700c1da
Samuel Warner <samwarner004@gmail.com>
2026-08-24 10:27:51 -0400

    updated url name

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import { getGame, loadProfile, getUserInformation, getOwnedGames, createNewTable
 import dotenv from 'dotenv'
 dotenv.config()
 
-const targetURL = process.env.API_URL;
+const targetURL = process.env.FRONTEND_URL;
 
 const app = express();
 
